cb6602a54bce80a4a1c7ea2d19fea2d224033a46
Muhammad-Saad-Haider <mohd.saadhaider@gmail.com>
2024-12-13 16:02:00 +0530

    added routes and fixed some typos

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { registerUser, loginUser, logoutUser, refreshAccessToken, changePassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js"
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changePassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannel, getWatchHistory } from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
@@ -26,11 +26,14 @@ router.route('/login').post(upload.none(), loginUser);  // 'upload.none()' => th
 
 router.route('/logout').post(verifyJWT ,logoutUser);
 router.route('/refresh-token').post(refreshAccessToken);
-router.route('/change-pasword').post(verifyJWT, changePassword);
+router.route('/change-password').post(verifyJWT, changePassword);
 router.route('/current-user').get(verifyJWT, getCurrentUser);
-router.route('/change-account-details').patch(verifyJWT,upload.none(), updateAccountDetails);
+router.route('/update-account-details').patch(verifyJWT,upload.none(), updateAccountDetails);
 
 router.route('/change-avatar').patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
-router.route('/change-cover-image').patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route('/change-coverImage').patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route('/c/:username').get(verifyJWT, getUserChannel);
+router.route('/history').get(verifyJWT, getWatchHistory);
 
 export default router
